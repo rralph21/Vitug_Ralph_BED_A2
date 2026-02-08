0c7f904a5bde9b2d5a1a61295825ef910d102094
Ralph.Vitug <rralph@rrc.ca>
2026-02-08 16:14:44 -0600

    add: gitIcketUrgencyById route

--- src/api/v1/routes/ticketRoutes.ts
+++ src/api/v1/routes/ticketRoutes.ts
@@ -5,6 +5,7 @@ import {
     createTicketById,
     updateTicketById,
     deleteTicketById,
+    getTicketUrgencyById
 } from "../controllers/ticketControllers";
 
 
@@ -15,5 +16,6 @@ router.get("/tickets/:id", getTicketByID); // get all tickets
 router.post("/tickets", createTicketById);
 router.put("/tickets/:id", updateTicketById);
 router.delete("/tickets/:id", deleteTicketById);
+router.get("/tickets/:id/urgency", getTicketUrgencyById);
 
 export default router;
