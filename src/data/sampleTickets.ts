8ef48ba4be4ae8f44ec0f3bba543f07cb8c9e25a
Ralph.Vitug <rralph@rrc.ca>
2026-02-08 13:33:45 -0600

    add: sampleTicket interface initialized and dample data added

--- src/data/sampleTickets.ts
+++ src/data/sampleTickets.ts
@@ -1,23 +1,69 @@
-// import express, { Express } from "express";
+import { sampleTickets } from "../api/v1/services/ticketServices"
 
-// const app: Express = express();
+export const tickers: sampleTickets[] = [
+    {
+        id: 1,
+        title: "Update footer copyright year",
+        description: "Footer still shows 2024",
+        priority: "low",
+        status: "open",
+        createdAt: new Date("2025-01-12T10:00:00.000Z")
+    },
 
-// app.get("/api/v1/items", (req, res) => {
-//     res.send("Get all items");
-// });
+    {
+        id: 2,
+        title: "Profile picture upload",
+        description: "slow Upload takes 30+ seconds",
+        priority: "medium",
+        status: "open",
+        createdAt: new Date('2025-01-13T10:00:00.000Z') 
+    },
 
-// app.post("/api/v1/items", (req, res) => {
-//     res.send("Create a new item");
-// });
+    {
+        id: 3,
+        title: "Dashboard loading slowly",
+        description: "Dashboard takes 10+ seconds to load",
+        priority: "medium",
+        status: "open",
+        createdAt: new Date('2025-01-09T10:00:00.000Z') 
+    },
 
-// app.put("/api/v1/items/:id", (req, res) => {
-//     res.send("Update an item");
-// });
+     {
+        id: 4,
+        title: "Password reset email delayed",
+        description: "Reset emails taking over 30 minutes",
+        priority: "high",
+        status: "open",
+        createdAt: new Date('2025-01-10T10:00:00.000Z') 
+    },
 
-// app.delete("/api/v1/items/:id", (req, res) => {
-//     res.send("Delete an item");
-// });
+     {
+        id: 5,
+        title: "Export to PDF not working",
+        description: "PDF export fails silently",
+        priority: "high",
+        status: "open",
+        createdAt: new Date('2025-01-06T10:00:00.000Z') 
+    },
 
-// app.listen(3000, () => {
-//     console.log("Server is running on port 3000");
-// });
+     {
+        id: 6,
+        title: "Login page not loading",
+        description: "Users report blank screen on login",
+        priority: "critical",
+        status: "open",
+        createdAt: new Date('2025-01-09T10:00:00.000Z') 
+    },
+
+     {
+        id: 7,
+        title: "Dark mode toggle broken",
+        description: "Dark mode doesn't persist after reflesh",
+        priority: "medium",
+        status: "resolved",
+        createdAt: new Date('2025-01-05T10:00:00.000Z') 
+    },
+]
+
+
+
